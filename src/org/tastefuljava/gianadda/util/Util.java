3497f3d4ec803b3d642bd72c0537d8955e3d2eed
Maurice Perry <maurice@perry.ch>
2014-11-16 23:19:44 +0100

    Initial commit

--- src/org/tastefuljava/gianadda/util/Util.java
+++ src/org/tastefuljava/gianadda/util/Util.java
@@ -11,7 +11,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.SimpleTimeZone;
 import java.util.TimeZone;
 import java.util.logging.Level;
 import java.util.logging.Logger;
