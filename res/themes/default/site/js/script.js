1c438d686a4ef18af46105600f696686e08082ca
maurice <maurice@192.168.1.100>
2015-09-18 12:16:31 +0200

    Initial commit

--- res/themes/default/site/js/script.js
+++ res/themes/default/site/js/script.js
@@ -1,9 +1,19 @@
-$(document).on("click", "a[href='#menu']", function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $("#menu").show();
-});
 
-$(document).on("click", "body", function() {
-    $("#menu").hide();
+$(function() {
+    $("a[href='#menu']").on("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $("#menu").slideDown(500);
+    });
+    $("body").on("click", function() {
+        $("#menu").slideUp(500);
+    });
+    $("a[href^='#']").on("click", function(e) {
+        e.preventDefault();
+        var seltor = $(this).attr("href"),
+            top = $(seltor).offset().top;
+        $('html, body').animate({
+            scrollTop: top-32
+        }, 1000);
+    });
 });
